fe733a9f122f678c84b4a7e42413b4faf633cf3d
Hansel Wave <arastafiel@gmail.com>
2018-06-10 22:15:38 -0600

    Handle two states buttons

--- scenarios/menu/button.py
+++ scenarios/menu/button.py
@@ -13,23 +13,26 @@ class Button(pygame.sprite.Sprite):
        height => the height the button should be
        folder => the folder where the images are in
     """
-    def __init__(self, x,y, base_file, transition_file, end_file , width=100, height=100, folder = "menu"):
-        #pygame Sprite class constructor
+    def __init__(self, x,y, base_file, transition_file, end_file=None, width=100, height=100, folder = "menu"):
+        # pygame Sprite class constructor
         pygame.sprite.Sprite.__init__(self)
 
         self.x = x
         self.y = y
         self.pos = (x, y)
 
-        #set the states images
+        # set the states images
         self.base = utils.load_image(base_file, folder, -1, (width, height))
         self.transition = utils.load_image(transition_file, folder, -1, (width, height))
-        self.end = utils.load_image(end_file, folder, -1, (width, height))
+        if end_file is not None:
+            self.end = utils.load_image(end_file, folder, -1, (width, height))
+            self.end_rect = self.end.get_rect(topleft=self.pos)
 
-        #define the rects for all the sprite's states, _rect.x and _rect.y set the position of the objects
+        # define the rects for all the sprite's states, _rect.x and _rect.y set the position of the objects
         self.base_rect = self.base.get_rect(topleft=self.pos)
         self.transition_rect = self.transition.get_rect(topleft=self.pos)
-        self.end_rect = self.end.get_rect(topleft=self.pos)
+
+        self.flag = False 
 
     def update(self):
         pass
@@ -37,5 +40,6 @@ class Button(pygame.sprite.Sprite):
     def flip(self):
         self.base = pygame.transform.flip(self.base, 180, 0)
         self.transition = pygame.transform.flip(self.transition, 180, 0)
-        self.end = pygame.transform.flip(self.end, 180, 0)
+        if end_file is not None:
+            self.end = pygame.transform.flip(self.end, 180, 0)
         return self
