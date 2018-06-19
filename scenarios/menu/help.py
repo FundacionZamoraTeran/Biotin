da210f3886b88abfe062b1216617c67328387383
Hansel Wave <arastafiel@gmail.com>
2018-06-18 21:03:49 -0600

    Update help modal to new style

--- scenarios/menu/help.py
+++ scenarios/menu/help.py
@@ -5,17 +5,19 @@ from scenarios.menu.button import Button
 class Help:
     def __init__(self, screen, clock):
         self.screen = screen
-        self.screen2 = pygame.Surface((900, 600),
+        self.screen2 = pygame.Surface((1200, 900),
                                       flags=pygame.SRCALPHA).convert_alpha()
         self.clock = clock
+        self.background = utils.load_image("help/background.png", "menu")
+        self.title = utils.load_image("help/title.png", "menu")
         self.tutorial = utils.load_image("help.png", "menu")
         self.exit_button = Button(1030,
-                                  140,
+                                  120,
                                   "exit.png",
                                   "exit.png",
                                   "exit.png",
-                                  30,
-                                  30)
+                                  36,
+                                  38)
 
     def run(self):
         """ control the actions happening on the Help modal"""
@@ -35,9 +37,9 @@ class Help:
                     elif  event.key == pygame.K_RETURN:
                         running = False
 
-            self.screen2.fill((250, 207, 149, 220))
-            self.screen2.blit(self.tutorial, (50, 0))
-            self.screen.blit(self.screen2, (150, 150))
-            self.screen.blit(self.exit_button.base, (1030, 140))
+            self.screen2.blit(self.background, (0, 0))
+            self.screen2.blit(self.title, (520, 160))
+            self.screen.blit(self.screen2, (0, 0))
+            self.screen.blit(self.exit_button.base, (1030, 120))
             pygame.display.flip()
             self.clock.tick(30)
