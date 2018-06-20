7ad0b3eb9ded3a8e00a1c7bda955793daf634bf5
Hansel Wave <arastafiel@gmail.com>
2018-06-20 13:55:59 -0600

    Advance options functionality

--- scenarios/menu/options.py
+++ scenarios/menu/options.py
@@ -1,3 +1,5 @@
+import json
+import sys
 import pygame
 from scenarios.utils import utils
 from scenarios.menu.button import Button
@@ -9,7 +11,31 @@ class Option:
                                       flags=pygame.SRCALPHA).convert_alpha()
         self.clock = clock
         self.background = utils.load_image("options_door/background.png", "menu")
-        #self.title = utils.load_image("options_door/title.png", "menu")
+        self.title = utils.load_image("options_door/title.png", "menu")
+        self.voice_title = Button(210,
+                                  350,
+                                  "options_door/vt1.png",
+                                  "options_door/vt2.png",
+                                  None,
+                                  157,
+                                  73)
+        self.voice_slider = utils.load_image("options_door/1.0.png", "menu")
+        self.music_title = Button(210,
+                                  480,
+                                  "options_door/mt1.png",
+                                  "options_door/mt2.png",
+                                  None,
+                                  157,
+                                  73)
+        self.music_slider = utils.load_image("options_door/0.4.png", "menu")
+        self.fx_title = Button(180,
+                               610,
+                               "options_door/ft1.png",
+                               "options_door/ft2.png",
+                               None,
+                               181,
+                               73)
+        self.fx_slider = utils.load_image("options_door/0.8.png", "menu")
         self.exit_button = Button(1030,
                                   120,
                                   "exit.png",
@@ -25,7 +51,14 @@ class Option:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    sys.exit(0)
+                if event.type == pygame.MOUSEMOTION:
+                    self.voice_title.on_selection_altern(self.screen, mouse_x, mouse_y)
+                    # Load Button
+                    self.music_title.on_selection_altern(self.screen, mouse_x, mouse_y)
+                    # Exit Button
+                    self.fx_title.on_selection_altern(self.screen, mouse_x, mouse_y)
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.exit_button.base_rect.collidepoint(mouse_x, mouse_y):
                         running = False
@@ -37,7 +70,13 @@ class Option:
                         running = False
 
             self.screen2.blit(self.background, (0, 0))
-            #self.screen2.blit(self.title, (520, 160))
+            self.screen2.blit(self.title, (460, 190))
+            self.screen2.blit(self.voice_title.base, (210, 350))
+            self.screen2.blit(self.voice_slider, (380, 350))
+            self.screen2.blit(self.music_title.base, (210, 480))
+            self.screen2.blit(self.music_slider, (380, 480))
+            self.screen2.blit(self.fx_title.base, (180, 610))
+            self.screen2.blit(self.fx_slider, (380, 610))
             self.screen.blit(self.screen2, (0, 0))
             self.screen.blit(self.exit_button.base, (1030, 120))
             pygame.display.flip()
