5f5b70b90617c16a47493b6b21485189dcc64277
Hansel Wave <arastafiel@gmail.com>
2018-08-30 01:39:10 -0600

    advance on saar; add ena's house to saar

--- scenarios/saar/entrance.py
+++ scenarios/saar/entrance.py
@@ -3,15 +3,17 @@ import pygame
 from scenarios.utils import utils
 from scenarios.utils import consts
 from scenarios.utils import saves
-from scenarios.utils.button import Button
+from scenarios.saar import hena
 from actors.player import Player
+from actors.enemy import Enemy
 from actors.platform import Platform
+from actors.prompt import Prompt
 
 
 class Entrance:
     """
-        Class representing the prologue comics before minigame, recieves
-        a Surface as a screen, and a Clock as clock
+        Class representing the first stage of Saar village, recieves
+        a Surface as a screen, and a Clock as clock, and a save slot name
     """
     def __init__(self, screen, clock, slot):
         self.screen = screen
@@ -22,17 +24,49 @@ class Entrance:
         self.fx_channel.set_volume(consts.FX_VOLUME)
         self.vx_channel = pygame.mixer.Channel(1)
         self.vx_channel.set_volume(consts.VX_VOLUME)
-        self.font = utils.load_font("notoregu.ttf", 30)
         self.next_level = 1
         self.character = "ena" if self.slot["team_ena"] is True else "ezer"
-
         self.background = utils.load_image("background.png", "saar/stage_1")
         self.background_width = self.background.get_size()[0]
         self.foreground = utils.load_image("foreground.png", "saar/stage_1")
-        self.ground = Platform(self.screen, self.clock, (0, 742), "ground.png", "saar/stage_1")
+        self.ground = Platform(self.screen,
+                               self.clock,
+                               (0, 742),
+                               "ground.png",
+                               "saar/stage_1")
+        self.interact = Prompt(self.screen,
+                               self.clock,
+                               (635, 480),
+                               "interact.png",
+                               "saar",
+                               (400, 600))
 
-        self.player = Player(self.screen, self.clock, (0, 640), self.character, 2400, True)
+        self.interact_2 = Prompt(self.screen,
+                                 self.clock,
+                                 (2155, 480),
+                                 "interact.png",
+                                 "saar",
+                                 (400, 600))
 
+        self.player = Player(self.screen,
+                             self.clock,
+                             (150, 640),
+                             self.character,
+                             2400,
+                             True)
+        self.gali = Enemy(self.screen,
+                          self.clock,
+                          (1000, 640),
+                          "monsters/gali",
+                          (300, 1000),
+                          6)
+        self.lopop = Enemy(self.screen,
+                           self.clock,
+                           (1940, 700),
+                           "monsters/lopop",
+                           (1300, 1940),
+                           8,
+                           35)
 
     def run(self):
         utils.load_bg("nocturne.ogg")
@@ -45,20 +79,20 @@ class Entrance:
             if rel_x < consts.WIDTH_SCREEN:
                 self.screen.blit(self.background, (rel_x, 0))
                 self.screen.blit(self.ground.image, (rel_x, 742))
-                self.player.update()
+                self.actors_load(abs(rel_x))
                 self.screen.blit(self.foreground, (rel_x, 585))
             else:
                 self.screen.blit(self.background, (rel_x - self.background_width, 0))
                 self.screen.blit(self.ground.image, (rel_x - self.background_width, 742))
-                self.player.update()
+                self.actors_load(rel_x)
                 self.screen.blit(self.foreground, (rel_x - self.background_width, 585))
-
             pygame.display.flip()
             self.clock.tick(30)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                    self.next_level = None
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         self.player.direction = "left"
@@ -66,8 +100,39 @@ class Entrance:
                     elif event.key == pygame.K_RIGHT:
                         self.player.direction = "right"
                         self.player.velocity = abs(self.player.velocity)
+                    elif event.key == pygame.K_DOWN:
+                        if (self.player.real_x+self.player.rect.width > 620
+                                and self.player.real_x+self.player.rect.width < 745):
+                            hus = hena.Hena(self.screen, self.clock, self.character)
+                            hus.run()
+                            del hus
+                            utils.load_bg("nocturne.ogg")
+                            pygame.mixer.music.set_volume(consts.BG_VOLUME-0.3)
+                            pygame.mixer.music.play(-1, 0.0)
+                        elif (self.player.real_x+self.player.rect.width > 2140 and
+                              self.player.real_x+self.player.rect.width < 2280):
+                            self.lopop.squashing = True # test to see if monster get squashed
+                            #hus = hcesar.Hcesar(self.screen, self.clock, self.character)
+                            #hus.run()
+                            #del hus
+                            #utils.load_bg("nocturne.ogg")
+                            #pygame.mixer.music.set_volume(consts.BG_VOLUME-0.3)
+                            #pygame.mixer.music.play(-1, 0.0)
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.player.direction = "stand"
                     elif event.key == pygame.K_RIGHT:
                         self.player.direction = "stand"
+
+    def actors_load(self, rel_x):
+        if self.player.real_x < 1500 and not self.gali.defeated:
+            self.gali.roam(rel_x)
+        if self.player.real_x > 1000 and not self.lopop.defeated:
+            self.lopop.roam(rel_x)
+        if (self.player.real_x+self.player.rect.width > 620
+                and self.player.real_x+self.player.rect.width < 745):
+            self.interact.float(rel_x)
+        if (self.player.real_x+self.player.rect.width > 2140
+                and self.player.real_x+self.player.rect.width < 2280):
+            self.interact_2.float(1200)
+        self.player.update()
---
+++ scenarios/saar/hena.py
@@ -0,0 +1,206 @@
+import sys
+import pygame
+
+from scenarios.utils import utils
+from scenarios.utils import consts
+from scenarios.utils import saves
+from scenarios.utils.button import Button
+from actors.player import Player
+from actors.prompt import Prompt
+
+
+class Hena:
+    """
+        Class representing Ena's house recieves
+        a Surface as a screen, and a Clock as clock, and a save slot name
+    """
+    def __init__(self, screen, clock, character):
+        self.screen = screen
+        self.clock = clock
+        self.fx_channel = pygame.mixer.Channel(0)
+        self.fx_channel.set_volume(consts.FX_VOLUME)
+        self.vx_channel = pygame.mixer.Channel(1)
+        self.vx_channel.set_volume(consts.VX_VOLUME)
+        self.background = utils.load_image("background.png", "saar/stage_1/hena")
+        self.character = character
+        self.interact = Prompt(self.screen,
+                               self.clock,
+                               (635, 280),
+                               "interact.png",
+                               "saar",
+                               (150, 350))
+
+        self.interact_2 = Prompt(self.screen,
+                                 self.clock,
+                                 (60, 380),
+                                 "interact.png",
+                                 "saar",
+                                 (250, 450))
+
+        self.player = Player(self.screen,
+                             self.clock,
+                             (80, 500),
+                             self.character,
+                             1200)
+        self.current_slide = 1
+        self.played = [0] * 15
+        self.father = utils.load_image("neutral.png", "npc/fena")
+        self.conversation = {
+            "1" : utils.load_image("d1.png", "saar/stage_1/hena"),
+            "2" : utils.load_image("d2.png", "saar/stage_1/hena"),
+            "3" : utils.load_image("d3.png", "saar/stage_1/hena"),
+            "4" : utils.load_image("d4.png", "saar/stage_1/hena"),
+            "5" : utils.load_image("d5.png", "saar/stage_1/hena"),
+            "6" : utils.load_image("d6.png", "saar/stage_1/hena"),
+            "7" : utils.load_image("d7.png", "saar/stage_1/hena"),
+            "8" : utils.load_image("d8.png", "saar/stage_1/hena"),
+            "9" : utils.load_image("d9.png", "saar/stage_1/hena"),
+            "10": utils.load_image("d10.png", "saar/stage_1/hena"),
+            "11": utils.load_image("d11.png", "saar/stage_1/hena"),
+            "12": utils.load_image("d12.png", "saar/stage_1/hena"),
+            "13": utils.load_image("d13.png", "saar/stage_1/hena"),
+            "14": utils.load_image("d14.png", "saar/stage_1/hena")
+        }
+        self.voices = {
+            "1" : utils.load_vx("saar/hena/1.ogg"),
+            "2" : utils.load_vx("saar/hena/2.ogg"),
+            "3" : utils.load_vx("saar/hena/3.ogg"),
+            "4" : utils.load_vx("saar/hena/4.ogg"),
+            "5" : utils.load_vx("saar/hena/5.ogg"),
+            "7" : utils.load_vx("saar/hena/7.ogg"),
+            "8" : utils.load_vx("saar/hena/8.ogg"),
+            "9" : utils.load_vx("saar/hena/9.ogg"),
+            "10": utils.load_vx("saar/hena/10.ogg"),
+            "11": utils.load_vx("saar/hena/11.ogg"),
+            "12": utils.load_vx("saar/hena/12.ogg"),
+            "13": utils.load_vx("saar/hena/13.ogg"),
+            "14": utils.load_vx("saar/hena/14.ogg")
+        }
+
+        self.biotin = {
+            "diego": utils.load_image("down1.png", "diego"),
+            "ena": utils.load_image("down1.png", "ena"),
+            "ezer": utils.load_image("down1.png", "ezer"),
+            "cesar": utils.load_image("down1.png", "cesar")
+        }
+
+        self.next = Button((1038, 780), "hena/next1.png", "hena/next2.png", 123, 94, "saar/stage_1")
+        self.prev = Button((55, 780), "hena/prev1.png", "hena/prev2.png", 123, 94, "saar/stage_1")
+
+    def run(self):
+        utils.load_bg("house.ogg")
+        pygame.mixer.music.set_volume(consts.BG_VOLUME-0.3)
+        pygame.mixer.music.play(-1, 0.0)
+        running = True
+
+        while running:
+            self.screen.blit(self.background, (0, 0))
+            self.screen.blit(self.father, (650, 380))
+            self.render_scene(self.current_slide)
+            pygame.display.flip()
+            self.clock.tick(30)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    sys.exit(0)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        if self.current_slide == 1:
+                            self.player.direction = "left"
+                            self.player.velocity = -abs(self.player.velocity)
+                        elif self.current_slide < 16:
+                            self.vx_channel.stop()
+                            self.prev.on_press(self.screen)
+                            self.played[self.current_slide-1] = 0
+                            self.current_slide -= 1
+                    elif event.key == pygame.K_RIGHT:
+                        if self.current_slide == 1:
+                            self.player.direction = "right"
+                            self.player.velocity = abs(self.player.velocity)
+                        elif self.current_slide > 1 and self.current_slide < 15:
+                            self.vx_channel.stop()
+                            self.next.on_press(self.screen)
+                            self.played[self.current_slide-1] = 0
+                            self.current_slide += 1
+                        elif self.current_slide == 15:
+                            self.current_slide = 1
+                    elif event.key == pygame.K_DOWN:
+                        if self.current_slide == 1:
+                            if (self.player.x+self.player.rect.width > 620 and
+                                    self.player.x+self.player.rect.width < 765
+                                    and self.current_slide == 1):
+                                self.current_slide += 1
+                            if (self.player.x+self.player.rect.width > 45
+                                    and self.player.x+self.player.rect.width < 175):
+                                running = False
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.player.direction = "stand"
+                    elif event.key == pygame.K_RIGHT:
+                        self.player.direction = "stand"
+
+    def render_scene(self, number):
+        if number == 1:
+            self.arrange_team(number)
+            if (self.player.x+self.player.rect.width > 620
+                    and self.player.x+self.player.rect.width < 765):
+                self.interact.float(0)
+            if (self.player.x+self.player.rect.width > 45
+                    and self.player.x+self.player.rect.width < 175):
+                self.interact_2.float(0)
+        elif number == 2:
+            if self.played[1] == 0:
+                self.vx_channel.play(self.voices["1"])
+                self.played[1] = 1
+            self.arrange_team(number)
+            self.screen.blit(self.conversation["1"], (0, 617))
+            self.screen.blit(self.next.base, (1038, 780))
+            self.screen.blit(self.prev.base, (55, 780))
+        elif number == 7:
+            self.arrange_team(number)
+            self.screen.blit(self.conversation["6"], (0, 617))
+            self.screen.blit(self.next.base, (1038, 780))
+            self.screen.blit(self.prev.base, (55, 780))
+        elif number < 16:
+            if self.played[number-1] == 0:
+                self.vx_channel.play(self.voices[str(number-1)])
+                self.played[number-1] = 1
+            self.arrange_team(number)
+            self.screen.blit(self.conversation[str(number-1)], (0, 617))
+            self.screen.blit(self.next.base, (1038, 780))
+            self.screen.blit(self.prev.base, (55, 780))
+
+    def arrange_team(self, number):
+        if self.character == "ena":
+            self.screen.blit(self.biotin["cesar"], (400, 500))
+            self.screen.blit(self.biotin["ezer"], (250, 450))
+            self.screen.blit(self.biotin["diego"], (180, 700))
+            if number == 1:
+                self.player.update()
+            else:
+                self.screen.blit(self.biotin["ena"], self.player.rect.topleft)
+        elif self.character == "ezer":
+            self.screen.blit(self.biotin["cesar"], (400, 500))
+            self.screen.blit(self.biotin["ena"], (250, 450))
+            self.screen.blit(self.biotin["diego"], (180, 700))
+            if number == 1:
+                self.player.update()
+            else:
+                self.screen.blit(self.biotin["ezer"], self.player.rect.topleft)
+        elif self.character == "diego":
+            self.screen.blit(self.biotin["cesar"], (400, 500))
+            self.screen.blit(self.biotin["ena"], (250, 450))
+            self.screen.blit(self.biotin["ezer"], (180, 700))
+            if number == 1:
+                self.player.update()
+            else:
+                self.screen.blit(self.biotin["diego"], self.player.rect.topleft)
+        elif self.character == "cesar":
+            self.screen.blit(self.biotin["diego"], (400, 460))
+            self.screen.blit(self.biotin["ezer"], (250, 450))
+            self.screen.blit(self.biotin["ena"], (180, 700))
+            if number == 1:
+                self.player.update()
+            else:
+                self.screen.blit(self.biotin["cesar"], self.player.rect.topleft)
