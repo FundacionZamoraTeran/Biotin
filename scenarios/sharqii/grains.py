aa2740845d9b6774d957afdb5b6126024397085b
Hansel Wave <arastafiel@gmail.com>
2018-09-03 22:19:40 -0600

    add sharqii entrance/start and grains scenarios

---
+++ scenarios/sharqii/grains.py
@@ -0,0 +1,281 @@
+import pygame
+
+from scenarios.utils import utils
+from scenarios.utils import consts
+from scenarios.utils import saves
+from scenarios.utils.button import Button
+from actors.player import Player
+from actors.enemy import Enemy
+from actors.prompt import Prompt
+
+
+class Grain:
+    """
+        Class representing the first stage of Saar village, recieves
+        a Surface as a screen, and a Clock as clock, and a save slot name
+    """
+    def __init__(self, screen, clock, character):
+        self.screen = screen
+        self.clock = clock
+        self.fx_channel = pygame.mixer.Channel(0)
+        self.fx_channel.set_volume(consts.FX_VOLUME)
+        self.vx_channel = pygame.mixer.Channel(1)
+        self.vx_channel.set_volume(consts.VX_VOLUME)
+        self.character = character
+        self.background = utils.load_image("background.jpg", "sharqii/grains")
+        self.background_width = self.background.get_size()[0]
+        self.interact = Prompt(self.screen,
+                               self.clock,
+                               (1287, 480),
+                               "maize.png",
+                               "sharqii/grains/prompts",
+                               (400, 600))
+        self.interact_2 = Prompt(self.screen,
+                                 self.clock,
+                                 (1592, 480), #prompt width/2  +10
+                                 "beans.png",
+                                 "sharqii/grains/prompts",
+                                 (300, 500))
+        self.interact_3 = Prompt(self.screen,
+                                 self.clock,
+                                 (1737, 480),
+                                 "oat.png",
+                                 "sharqii/grains/prompts",
+                                 (400, 600))
+        self.interact_4 = Prompt(self.screen,
+                                 self.clock,
+                                 (1867, 480),
+                                 "quinoa.png",
+                                 "sharqii/grains/prompts",
+                                 (400, 600))
+        self.interact_5 = Prompt(self.screen,
+                                 self.clock,
+                                 (2027, 480),
+                                 "rice.png",
+                                 "sharqii/grains/prompts",
+                                 (400, 600))
+        self.interact_6 = Prompt(self.screen,
+                                 self.clock,
+                                 (980, 480),
+                                 "interact.png",
+                                 "sharqii",
+                                 (400, 600))#this is the key part
+        self.interact_7 = Prompt(self.screen,
+                                 self.clock,
+                                 (2280, 480),
+                                 "interact.png",
+                                 "sharqii",
+                                 (400, 600))
+
+        self.player = Player(self.screen,
+                             self.clock,
+                             (150, 480),
+                             self.character,
+                             2400,
+                             True)
+        self.current_slide = 1
+        self.played = [0] * 14
+        self.conversation = {
+            "1" : utils.load_image("d1.png", "sharqii/grains/"),
+            "2" : utils.load_image("d2.png", "sharqii/grains/"),
+            "3" : utils.load_image("d3.png", "sharqii/grains/"),
+            "4" : utils.load_image("oat.png", "sharqii/grains/"),
+            "5" : utils.load_image("maize.png", "sharqii/grains/"),
+            "6" : utils.load_image("quinoa.png", "sharqii/grains/"),
+            "7" : utils.load_image("rice.png", "sharqii/grains/"),
+            "8" : utils.load_image("beans.png", "sharqii/grains/"),
+            "9" : utils.load_image("key.png", "sharqii/grains/")
+        }
+        self.voices = {
+            "1" : utils.load_vx("sharqii/grains/1.ogg"),
+            "2" : utils.load_vx("sharqii/grains/2.ogg"),
+            "3" : utils.load_vx("sharqii/grains/3.ogg"),
+            "4" : utils.load_vx("sharqii/grains/a2.ogg"),
+            "5" : utils.load_vx("sharqii/grains/a1.ogg"),
+            "6" : utils.load_vx("sharqii/grains/a3.ogg"),
+            "7" : utils.load_vx("sharqii/grains/a4.ogg"),
+            "8" : utils.load_vx("sharqii/grains/a5.ogg")
+        }
+        self.biotin = {
+            "diego": utils.load_image("down1.png", "diego"),
+            "ena": utils.load_image("down1.png", "ena"),
+            "ezer": utils.load_image("down1.png", "ezer"),
+            "cesar": utils.load_image("down1.png", "cesar")
+        }
+        self.next = Button((1038, 780), "next1.png", "next2.png", 123, 94, "sharqii")
+        self.prev = Button((55, 780), "prev1.png", "prev2.png", 123, 94, "sharqii")
+        self.visited = [False] * 6
+
+    def run(self):
+        utils.load_bg("bazar.ogg")
+        pygame.mixer.music.set_volume(consts.BG_VOLUME)
+        pygame.mixer.music.play(-1, 0.0)
+        running = True
+
+        while running:
+            rel_x = self.player.stage["x"]
+            if rel_x < consts.WIDTH_SCREEN:
+                self.screen.blit(self.background, (rel_x, 0))
+                self.render_scene(self.current_slide, abs(rel_x))
+            else:
+                self.screen.blit(self.background, (rel_x - self.background_width, 0))
+                self.render_scene(self.current_slide, rel_x)
+            pygame.display.flip()
+            self.clock.tick(consts.FPS)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    self.next_level = None
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
+                        if self.current_slide == 1:
+                            self.vx_channel.stop()
+                            self.prev.on_press(self.screen)
+                            self.played[0] = 0
+                            self.current_slide = 10
+                        elif self.current_slide < 4:
+                            self.vx_channel.stop()
+                            self.prev.on_press(self.screen)
+                            self.played[self.current_slide-1] = 0
+                            self.current_slide -= 1
+                        elif 3 < self.current_slide < 10:
+                            self.vx_channel.stop()
+                            self.prev.on_press(self.screen)
+                            self.played[self.current_slide-1] = 0
+                            self.current_slide = 10
+                        elif self.current_slide == 10:
+                            self.player.direction = "left"
+                            self.player.velocity = -abs(self.player.velocity)
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_KP6:
+                        if self.current_slide < 3:
+                            self.vx_channel.stop()
+                            self.next.on_press(self.screen)
+                            self.played[self.current_slide-1] = 0
+                            self.current_slide += 1
+                        elif 2 < self.current_slide < 10:
+                            self.vx_channel.stop()
+                            self.next.on_press(self.screen)
+                            self.played[self.current_slide-1] = 0
+                            self.current_slide = 10
+                        elif self.current_slide == 10:
+                            self.player.direction = "right"
+                            self.player.velocity = abs(self.player.velocity)
+                    elif event.key == pygame.K_DOWN or event.key == pygame.K_KP2:
+                        if self.current_slide == 10:
+                            if (self.player.real_x+self.player.rect.width > 1272
+                                    and self.player.real_x+self.player.rect.width < 1433):
+                                self.current_slide = 5
+                                self.visited[0] = True
+                            if (self.player.real_x+self.player.rect.width > 1577
+                                    and self.player.real_x+self.player.rect.width < 1728):
+                                self.current_slide = 8
+                                self.visited[1] = True
+                            if (self.player.real_x+self.player.rect.width > 1722
+                                    and self.player.real_x+self.player.rect.width < 1850):
+                                self.current_slide = 4
+                                self.visited[2] = True
+                            if (self.player.real_x+self.player.rect.width > 1852
+                                    and self.player.real_x+self.player.rect.width < 2010):
+                                self.current_slide = 6
+                                self.visited[3] = True
+                            if (self.player.real_x+self.player.rect.width > 2012
+                                    and self.player.real_x+self.player.rect.width < 2173):
+                                self.current_slide = 7
+                                self.visited[4] = True
+                            if (self.player.real_x+self.player.rect.width > 980
+                                    and self.player.real_x+self.player.rect.width < 1126
+                                    and len(filter(lambda x: x is False, self.visited)) == 1):
+                                self.current_slide = 9
+                                self.visited[5] = True
+                            elif (self.player.real_x+self.player.rect.width > 2280
+                                  and self.player.real_x+self.player.rect.width < 2401 and
+                                  all(i is True for i in self.visited)):
+                                pass
+                    elif ((event.key == pygame.K_SPACE or event.key == pygame.K_PAGEDOWN) and
+                          (self.player.jumping is False and self.player.jump_frames == 0)):
+                        if self.current_slide == 10:
+                            self.player.jumping = True
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
+                        if self.current_slide == 10:
+                            self.player.direction = "stand"
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_KP6:
+                        if self.current_slide == 10:
+                            self.player.direction = "stand"
+    def render_scene(self, number, rel_x):
+        if 1 <= number < 9:
+            if self.played[number-1] == 0:
+                self.vx_channel.play(self.voices[str(number)])
+                self.played[number-1] = 1
+            self.actors_load(number, rel_x)
+            self.screen.blit(self.conversation[str(number)], (0, 617))
+            self.screen.blit(self.next.base, (1038, 780))
+            self.screen.blit(self.prev.base, (55, 780))
+        elif number == 9:
+            self.actors_load(number, rel_x)
+            self.screen.blit(self.conversation["9"], (0, 617))
+            self.screen.blit(self.next.base, (1038, 780))
+        elif number == 10:
+            self.actors_load(number, rel_x)
+
+    def actors_load(self, number, rel_x):
+        if (self.player.real_x+self.player.rect.width > 1272
+                and self.player.real_x+self.player.rect.width < 1433):
+            self.interact.float(rel_x)
+        if (self.player.real_x+self.player.rect.width > 1577
+                and self.player.real_x+self.player.rect.width < 1728):
+            self.interact_2.float(rel_x)
+        if (self.player.real_x+self.player.rect.width > 1722
+                and self.player.real_x+self.player.rect.width < 1850):
+            self.interact_3.float(rel_x)
+        if (self.player.real_x+self.player.rect.width > 1852
+                and self.player.real_x+self.player.rect.width < 2010):
+            self.interact_4.float(1200)
+        if (self.player.real_x+self.player.rect.width > 2012
+                and self.player.real_x+self.player.rect.width < 2173):
+            self.interact_5.float(1200)
+        if (self.player.real_x+self.player.rect.width > 980
+                and self.player.real_x+self.player.rect.width < 1126
+                and len(filter(lambda x: x is False, self.visited)) == 1):
+            self.interact_6.float(rel_x)
+        if (self.player.real_x+self.player.rect.width > 2280
+                and self.player.real_x+self.player.rect.width < 2401 and
+                all(i is True for i in self.visited)):
+            self.interact_7.float(1200)
+        self.arrange_team(number, rel_x)
+
+    def arrange_team(self, number, rel_x):
+        if self.character == "ena":
+            self.screen.blit(self.biotin["ezer"], (420-rel_x, 495))
+            self.screen.blit(self.biotin["cesar"], (500-rel_x, 500))
+            self.screen.blit(self.biotin["diego"], (330-rel_x, 480))
+            if number == 10: # this check exist to blit the static image when the dialogue appears
+                self.player.update()
+            else:
+                self.screen.blit(self.biotin["ena"], self.player.rect.topleft)
+        elif self.character == "ezer":
+            self.screen.blit(self.biotin["ena"], (420-rel_x, 495))
+            self.screen.blit(self.biotin["cesar"], (500-rel_x, 500))
+            self.screen.blit(self.biotin["diego"], (330-rel_x, 480))
+            if number == 10:
+                self.player.update()
+            else:
+                self.screen.blit(self.biotin["ezer"], self.player.rect.topleft)
+        elif self.character == "diego":
+            self.screen.blit(self.biotin["cesar"], (400-rel_x, 495))
+            self.screen.blit(self.biotin["ena"], (480-rel_x, 500))
+            self.screen.blit(self.biotin["ezer"], (310-rel_x, 480))
+            if number == 10:
+                self.player.update()
+            else:
+                self.screen.blit(self.biotin["diego"], self.player.rect.topleft)
+        elif self.character == "cesar":
+            self.screen.blit(self.biotin["diego"], (420-rel_x, 495))
+            self.screen.blit(self.biotin["ezer"], (500-rel_x, 500))
+            self.screen.blit(self.biotin["ena"], (330-rel_x, 480))
+            if number == 10:
+                self.player.update()
+            else: 
+                self.screen.blit(self.biotin["cesar"], self.player.rect.topleft)
--- scenarios/sharqii/start.py
+++ scenarios/sharqii/start.py
@@ -1,100 +1,102 @@
-import sys
 import pygame
 
 from scenarios.utils import utils
 from scenarios.utils import consts
 from scenarios.utils import saves
 from scenarios.utils.button import Button
+from scenarios.sharqii import grains
 from actors.player import Player
 from actors.prompt import Prompt
 
 
-class Hcesar:
+class Entrance:
     """
-        Class representing Cesar's house recieves
+        Class representing the entrance of the great bazaar, recieves
         a Surface as a screen, and a Clock as clock, and a save slot name
     """
-    def __init__(self, screen, clock, character):
+    def __init__(self, screen, clock, slot):
         self.screen = screen
         self.clock = clock
+        self.slotname = slot
+        self.slot = saves.load_slot(slot)
         self.fx_channel = pygame.mixer.Channel(0)
         self.fx_channel.set_volume(consts.FX_VOLUME)
         self.vx_channel = pygame.mixer.Channel(1)
         self.vx_channel.set_volume(consts.VX_VOLUME)
-        self.background = utils.load_image("background.png", "saar/stage_1/hcesar")
-        self.character = character
-        self.interact = Prompt(self.screen,
-                               self.clock,
-                               (735, 280),
-                               "interact.png",
-                               "saar",
-                               (150, 350))
-
-        self.interact_2 = Prompt(self.screen,
-                                 self.clock,
-                                 (60, 380),
-                                 "interact.png",
-                                 "saar",
-                                 (250, 450))
-
+        self.next_level = 1
+        self.character = "ena" if self.slot["team_ena"] is True else "diego"
+        self.background = utils.load_image("background.png", "sharqii/entrance")
         self.player = Player(self.screen,
                              self.clock,
-                             (80, 500),
+                             (150, 500),
                              self.character,
-                             1200)
+                             1200,
+                             False)
+        self.interact = Prompt(self.screen,
+                               self.clock,
+                               (745, 280),
+                               "interact.png",
+                               "sharqii",
+                               (150, 350))
+        self.interact_2 = Prompt(self.screen,
+                                 self.clock,
+                                 (540, 380),
+                                 "interact.png",
+                                 "sharqii",
+                                 (250, 450))
         self.current_slide = 1
         self.played = [0] * 14
-        self.father = utils.load_image("happy.png", "npc/fcesar")
+        self.pilori = utils.load_image("pilori.png", "sharqii/entrance")
         self.conversation = {
-            "1" : utils.load_image("d1.png", "saar/stage_1/hcesar"),
-            "2" : utils.load_image("d2.png", "saar/stage_1/hcesar"),
-            "3" : utils.load_image("d3.png", "saar/stage_1/hcesar"),
-            "4" : utils.load_image("d4.png", "saar/stage_1/hcesar"),
-            "5" : utils.load_image("d5.png", "saar/stage_1/hcesar"),
-            "6" : utils.load_image("d6.png", "saar/stage_1/hcesar"),
-            "7" : utils.load_image("d7.png", "saar/stage_1/hcesar"),
-            "8" : utils.load_image("d8.png", "saar/stage_1/hcesar"),
-            "9" : utils.load_image("d9.png", "saar/stage_1/hcesar"),
-            "10": utils.load_image("d10.png", "saar/stage_1/hcesar"),
-            "11": utils.load_image("d11.png", "saar/stage_1/hcesar"),
-            "12": utils.load_image("d12.png", "saar/stage_1/hcesar"),
-            "13": utils.load_image("d13.png", "saar/stage_1/hcesar")
+            "1" : utils.load_image("d1.png", "sharqii/entrance/"),
+            "2" : utils.load_image("d2.png", "sharqii/entrance/"),
+            "3" : utils.load_image("d3.png", "sharqii/entrance/"),
+            "4" : utils.load_image("d4.png", "sharqii/entrance/"),
+            "5" : utils.load_image("d5.png", "sharqii/entrance/"),
+            "6" : utils.load_image("d6.png", "sharqii/entrance/"),
+            "7" : utils.load_image("d7.png", "sharqii/entrance/"),
+            "8" : utils.load_image("d8.png", "sharqii/entrance/"),
+            "9" : utils.load_image("d9.png", "sharqii/entrance/"),
+            "10": utils.load_image("d10.png", "sharqii/entrance/"),
+            "11": utils.load_image("d11.png", "sharqii/entrance/"),
+            "12": utils.load_image("d12.png", "sharqii/entrance/"),
+            "13": utils.load_image("d13.png", "sharqii/entrance/")
         }
         self.voices = {
-            "1" : utils.load_vx("saar/hcesar/1.ogg"),
-            "2" : utils.load_vx("saar/hcesar/2.ogg"),
-            "3" : utils.load_vx("saar/hcesar/3.ogg"),
-            "4" : utils.load_vx("saar/hcesar/4.ogg"),
-            "5" : utils.load_vx("saar/hcesar/5.ogg"),
-            "6" : utils.load_vx("saar/hcesar/6.ogg"),
-            "7" : utils.load_vx("saar/hcesar/7.ogg"),
-            "8" : utils.load_vx("saar/hcesar/8.ogg"),
-            "9" : utils.load_vx("saar/hcesar/9.ogg"),
-            "10": utils.load_vx("saar/hcesar/10.ogg"),
-            "11": utils.load_vx("saar/hcesar/11.ogg"),
-            "12": utils.load_vx("saar/hcesar/12.ogg"),
-            "13": utils.load_vx("saar/hcesar/13.ogg")
+            "1" : utils.load_vx("sharqii/entrance/1.ogg"),
+            "2" : utils.load_vx("sharqii/entrance/2.ogg"),
+            "3" : utils.load_vx("sharqii/entrance/3.ogg"),
+            "4" : utils.load_vx("sharqii/entrance/4.ogg"),
+            "5" : utils.load_vx("sharqii/entrance/5.ogg"),
+            "6" : utils.load_vx("sharqii/entrance/6.ogg"),
+            "7" : utils.load_vx("sharqii/entrance/7.ogg"),
+            "8" : utils.load_vx("sharqii/entrance/8.ogg"),
+            "9" : utils.load_vx("sharqii/entrance/9.ogg"),
+            "10": utils.load_vx("sharqii/entrance/10.ogg"),
+            "11": utils.load_vx("sharqii/entrance/11.ogg"),
+            "12": utils.load_vx("sharqii/entrance/12.ogg"),
+            "13": utils.load_vx("sharqii/entrance/13.ogg")
         }
-
         self.biotin = {
             "diego": utils.load_image("down1.png", "diego"),
             "ena": utils.load_image("down1.png", "ena"),
             "ezer": utils.load_image("down1.png", "ezer"),
             "cesar": utils.load_image("down1.png", "cesar")
         }
+        self.next = Button((1038, 780), "next1.png", "next2.png", 123, 94, "sharqii")
+        self.prev = Button((55, 780), "prev1.png", "prev2.png", 123, 94, "sharqii")
+        self.visited = False
 
-        self.next = Button((1038, 780), "hcesar/next1.png", "hcesar/next2.png", 123, 94, "saar/stage_1")
-        self.prev = Button((55, 780), "hcesar/prev1.png", "hcesar/prev2.png", 123, 94, "saar/stage_1")
 
     def run(self):
-        utils.load_bg("house.ogg")
+        utils.load_bg("bazar.ogg")
         pygame.mixer.music.set_volume(consts.BG_VOLUME-0.3)
         pygame.mixer.music.play(-1, 0.0)
         running = True
 
         while running:
             self.screen.blit(self.background, (0, 0))
-            self.screen.blit(self.father, (750, 370))
+            self.screen.blit(self.pilori, (725, 370))
             self.render_scene(self.current_slide)
             pygame.display.flip()
             self.clock.tick(consts.FPS)
@@ -102,7 +104,7 @@ class Hcesar:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                    sys.exit(0)
+                    self.next_level = None
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
                         if self.current_slide == 1:
@@ -127,13 +129,23 @@ class Hcesar:
                             self.current_slide = 1
                     elif event.key == pygame.K_DOWN or event.key == pygame.K_KP2:
                         if self.current_slide == 1:
-                            if (self.player.rect.x+self.player.rect.width > 720 and
-                                    self.player.rect.x+self.player.rect.width < 865
+                            if (self.player.rect.x+self.player.rect.width > 730 and
+                                    self.player.rect.x+self.player.rect.width < 875
                                     and self.current_slide == 1):
                                 self.current_slide += 1
-                            if (self.player.rect.x+self.player.rect.width > 45
-                                    and self.player.rect.x+self.player.rect.width < 175):
+                                self.visited = True
+                            if (self.player.rect.x+self.player.rect.width > 525
+                                and self.player.rect.x+self.player.rect.width < 655 and
+                                self.visited is True):
+                                utils.loading_screen(self.screen)
+                                gra = grains.Grain(self.screen, self.clock, self.character)
+                                gra.run()
+                                del gra
                                 running = False
+                                utils.loading_screen(self.screen)
+                                #save here
+                                if not self.slot["stages"]["bazar"] is True:
+                                    saves.save(self.slotname, 4, "El gran bazar", "bazar")
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
                         self.player.direction = "stand"
@@ -144,11 +156,12 @@ class Hcesar:
     def render_scene(self, number):
         if number == 1:
             self.arrange_team(number)
-            if (self.player.rect.x+self.player.rect.width > 720
-                    and self.player.rect.x+self.player.rect.width < 865):
+            if (self.player.rect.x+self.player.rect.width > 730
+                    and self.player.rect.x+self.player.rect.width < 875):
                 self.interact.float(0)
-            if (self.player.rect.x+self.player.rect.width > 45
-                    and self.player.rect.x+self.player.rect.width < 175):
+            if (self.player.rect.x+self.player.rect.width > 525
+                    and self.player.rect.x+self.player.rect.width < 655 and
+                    self.visited is True):
                 self.interact_2.float(0)
         elif number == 2:
             if self.played[1] == 0:
@@ -185,9 +198,9 @@ class Hcesar:
             else:
                 self.screen.blit(self.biotin["ezer"], self.player.rect.topleft)
         elif self.character == "diego":
-            self.screen.blit(self.biotin["cesar"], (420, 495))
-            self.screen.blit(self.biotin["ena"], (500, 500))
-            self.screen.blit(self.biotin["ezer"], (330, 480))
+            self.screen.blit(self.biotin["cesar"], (400, 495))
+            self.screen.blit(self.biotin["ena"], (480, 500))
+            self.screen.blit(self.biotin["ezer"], (310, 480))
             if number == 1:
                 self.player.update()
             else:
