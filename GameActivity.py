b53101423bdad07fa4330e07734e6aed58345f02
Hansel Wave <arastafiel@gmail.com>
2018-09-04 23:41:35 -0600

    Add better compatibility with sugar; create valley scenario p2

--- GameActivity.py
+++ GameActivity.py
@@ -1,57 +1,52 @@
-#GameActivity.py
-
 from gettext import gettext as _
 
+import sys
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import pygame
 
-from sugar3.activity import activity
-from sugar3.graphics.toolbutton import ToolButton
-from sugar3.graphics.toolbarbox import ToolbarButton
+from sugar3.activity.activity import Activity
 from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.graphics.toolbutton import ToolButton
 from sugar3.activity.widgets import StopButton
-from sugar3.activity.widgets import ActivityToolbar
-from scenarios.utils import consts
-from gi.repository import GObject
 
 import sugargame.canvas
+
 import main
 
-class GameActivity(activity.Activity):
+class GameActivity(Activity):
     def __init__(self, handle):
-        super(GameActivity, self).__init__(handle)
+        Activity.__init__(self, handle)
+
+        self.game = main.Biotin()
 
         # Build the activity toolbar.
         self.build_toolbar()
 
-        SCREEN = pygame.display.set_mode(consts.RESOLUTION)
-        pygame.display.set_caption("Biotin: Una aventura energizante")
-        self.game = main.Biotin(SCREEN)
-
-        # Build the Pygame canvas.
-        self._pygamecanvas = sugargame.canvas.PygameCanvas(self)
+        # Build the Pygame canvas and start the game running
+        # (self.game.run is called when the activity constructor
+        # returns).
+        self._pygamecanvas = sugargame.canvas.PygameCanvas(self,
+            main=self.game.run,
+            modules=[pygame.display])
 
         # Note that set_canvas implicitly calls read_file when
         # resuming from the Journal.
         self.set_canvas(self._pygamecanvas)
         self._pygamecanvas.grab_focus()
 
-        # Start the game running (self.game.run is called when the
-        # activity constructor returns).
-        self._pygamecanvas.run_pygame(self.game.loop())
-
     def build_toolbar(self):
         toolbar_box = ToolbarBox()
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show()
 
-        view_toolbar = ViewToolbar()
-        view_toolbar.connect('go-fullscreen',
-                             self.view_toolbar_go_fullscreen_cb)
-        view_toolbar.show()
-        view_toolbar_button = ToolbarButton(
-            page=view_toolbar,
-            icon_name='toolbar-view')
-        toolbar_box.toolbar.insert(view_toolbar_button, -1)
-        view_toolbar_button.show()
+        activity_button = ActivityToolbarButton(self)
+        toolbar_box.toolbar.insert(activity_button, -1)
+        activity_button.show()
+
+        # Blank space (separator) and Stop button at the end:
 
         separator = Gtk.SeparatorToolItem()
         separator.props.draw = False
@@ -60,34 +55,18 @@ class GameActivity(activity.Activity):
         separator.show()
 
         stop_button = StopButton(self)
-        stop_button.props.accelerator = '<Ctrl><Shift>Q'
         toolbar_box.toolbar.insert(stop_button, -1)
         stop_button.show()
+        stop_button.connect('clicked', self._stop_cb)
 
-        self.set_toolbar_box(toolbar_box)
-        toolbar_box.show()
+    def _stop_cb(self, button):
+        self.game.running = False
 
-    def view_toolbar_go_fullscreen_cb(self, view_toolbar):
-        self.fullscreen()
+    def read_file(self, file_path):
+        pass
 
-class ViewToolbar(Gtk.Toolbar):
-    __gtype_name__ = 'ViewToolbar'
+    def write_file(self, file_path):
+        pass
 
-    __gsignals__ = {
-        'needs-update-size': (GObject.SIGNAL_RUN_FIRST,
-                              GObject.TYPE_NONE,([])),
-        'go-fullscreen': (GObject.SIGNAL_RUN_FIRST,
-                          GObject.TYPE_NONE,
-                          ([]))
-    }
-
-    def __init__(self):
-        Gtk.Toolbar.__init__(self)
-        self.fullscreen = ToolButton('view-fullscreen')
-        self.fullscreen.set_tooltip(_('Fullscreen'))
-        self.fullscreen.connect('clicked', self.fullscreen_cb)
-        self.insert(self.fullscreen, -1)
-        self.fullscreen.show()
-
-    def fullscreen_cb(self, button):
-        self.emit('go-fullscreen')
+    def get_preview(self):
+        return self._pygamecanvas.get_preview()
--- main2.py
+++ main2.py
@@ -2,20 +2,23 @@
 import sys
 import logging
 import pygame
+#import cProfile as profile
 
 from pygame.locals import *
+from gi.repository import Gtk
 from scenarios.menu import menu
 from scenarios.house import outside
 from scenarios.map import mapp
 from scenarios.sharqii import start
 from scenarios.saar import entrance
 from scenarios.rahapara import bowling
+from scenarios.valley import hill
 from scenarios.utils import consts
 from actors import player
 
 pygame.mixer.pre_init(44100, -16, 4, 2048)
 pygame.mixer.init()
-pygame.font.init()
+pygame.init() #comment only when using GameActivity.py
 
 LOG = logging.getLogger('activity.b7')
 logging.basicConfig()
@@ -25,9 +28,10 @@ class Biotin:
         Main class that handle the game loop
     """
     running = True
-    def __init__(self):
+    def __init__(self, screen):
         self.actor = None
         self.clock = None
+        self.screen = screen
         self.running = True
         self.next_level = 0
         self.levels = {
@@ -35,7 +39,8 @@ class Biotin:
             "1": mapp.Map,
             "2": entrance.Entrance,
             "3": bowling.Bowling,
-            "4": start.Entrance
+            "4": start.Entrance,
+            "5": hill.Hill
         }
 
     def reset_clock(self):
@@ -44,24 +49,35 @@ class Biotin:
     def quit(self):
         self.running = False
 
-    def run(self):
-        screen = pygame.display.get_surface()
-        pygame.event.set_blocked([MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN])
+    def loop(self):
         self.reset_clock()
         self.clock.tick(consts.FPS)
-        meny = menu.Menu(screen, self.clock)
-        meny.run()
-        self.next_level = meny.level_selected
-        slot = meny.slot_selected
-        del meny
-        while self.next_level is not None:
-            self.level_selector(screen, self.next_level, slot)
+        # meny = menu.Menu(self.screen, self.clock)
+        # meny.run()
+        # self.next_level = meny.level_selected
+        # slot = meny.slot_selected
+        # del meny
+        # while self.next_level is not None:
+        #     self.level_selector(self.next_level, slot)
+        self.level_selector(5, "slot_2")
         pygame.quit()
         sys.exit(0)
 
-    def level_selector(self, screen, level, slot):
+    def level_selector(self, level, slot):
         if level is not None and slot is not None:
             #here we should load against a dict the selected level
-            var = self.levels[str(level)](screen, self.clock, slot)
+            var = self.levels[str(level)](self.screen, self.clock, slot)
             var.run()
             self.next_level = var.next_level
+
+
+if __name__ == "__main__":
+    SCREEN = pygame.display.set_mode(consts.RESOLUTION)
+    pygame.display.set_caption("Biotin: Una aventura energizante")
+    pygame.event.set_blocked(MOUSEMOTION)
+    pygame.event.set_blocked(MOUSEBUTTONUP)
+    pygame.event.set_blocked(MOUSEBUTTONDOWN)
+    pygame.event.set_blocked(VIDEORESIZE)
+    BIOTIN = Biotin(SCREEN)
+    BIOTIN.loop()
+    #profile.run('BIOTIN.loop()')
